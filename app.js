88af744e9bec341b996a125144ff4700a9e7a243
a.jaouen <a.jaouen@habitat-humanisme.org>
2020-11-05 14:49:04 +0100

    Arborescence 2

--- app.js
+++ app.js
@@ -4,13 +4,13 @@ const app = express()
 app.use(express.json())
 
 // DB
-const connect = require('./db/mongodb');
+const connect = require('./database/mongodb');
 //connect()
 
 // API
-require('./api/admin.js')(app)
-require('./api/athlete.js')(app)
-require('./api/sport.js')(app)
+require('./api/admin.api.js')(app)
+require('./api/athlete.api.js')(app)
+require('./api/sport.api.js')(app)
 
 // Default
 app.get('/', (req,res) => {
